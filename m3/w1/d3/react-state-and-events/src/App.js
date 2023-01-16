b346d3c83b38fd52c4ab7be13f0c755569295af8
Arcadio REYES <arcadioreyesph@gmail.com>
2023-01-16 17:49:31 +0100

    m3 w1 d2 code lesson react state hook and events done

--- m3/w1/d3/react-state-and-events/src/App.js
+++ m3/w1/d3/react-state-and-events/src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import Counter from "./components/Counter";
+import React, { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = (event) => {
+    setTheme(event.target.value);
+  };
+
+  return (
+    <div className={"App " + theme}>
+      <h1>React - state and events</h1>
+      <Counter />
+
+      <select onChange={toggleTheme}>
+        <option value="light">Light </option>
+        <option value="dark">Dark </option>
+      </select>
+    </div>
+  );
 }
 
 export default App;
