47525ab59a8975816d92cc349129e638d3b1e285
Arcadio REYES <arcadioreyesph@gmail.com>
2023-01-16 21:26:20 +0100

    m3 w1 d3 code lesson react list & keys done

---
+++ m3/w1/d3/react-lists/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+// import ProjectList from "./components/ProjectList";
+// import SimpleList from "./components/SimpleList";
+// import StudentList from "./components/StudentList";
+import MovieList from "./components/MovieList";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <SimpleList />
+      <StudentList />
+      <ProjectList /> */}
+      <MovieList />
+    </div>
+  );
+}
+
+export default App;
