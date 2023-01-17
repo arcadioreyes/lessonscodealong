77b369d7c773ba375671103b2d149e76fcb01408
Arcadio REYES <arcadioreyesph@gmail.com>
2023-01-17 22:21:27 +0100

    m3 w2 d1 recording lifting the State up lecture

---
+++ m3/w2/d1/lloyd-react-lifting-state-up/src/components/Task.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function Task(props) {
+  const { task, toggleTaskDone } = props;
+  //   const [taskCompleted, setTaskCompleted] = useState(false);
+
+  //   const toggleTask = () => setTaskCompleted(!taskCompleted);
+
+  return (
+    <div className="task-card">
+      <div className="task-card-half">
+        <h1>{task.name}</h1>
+        {task.isDone ? <span>DONE </span> : <span>PENDING ⌛</span>}
+
+        <h2>Task Description</h2>
+        <p>{task.description}</p>
+
+        <button className="add" onClick={() => toggleTaskDone(task._id)}>
+          {task.isDone ? <span>UNDO </span> : <span>✔️</span>}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Task;
