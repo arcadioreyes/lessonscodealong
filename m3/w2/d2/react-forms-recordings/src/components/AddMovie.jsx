62100e01dc18df1055398d6f2ca4d8d832a8b309
Arcadio REYES <arcadioreyesph@gmail.com>
2023-01-30 17:43:17 +0100

    m3 w2 d2 recording controlled forms lecture part II

--- m3/w2/d2/react-forms-recordings/src/components/AddMovie.jsx
+++ m3/w2/d2/react-forms-recordings/src/components/AddMovie.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 function AddMovie(props) {
+  const { addNewMovie } = props;
   const [title, setTitle] = useState("");
   const [director, setDirector] = useState("");
   const [IMDBRating, setIMDBRating] = useState(5);
@@ -9,7 +10,16 @@ function AddMovie(props) {
   const handleSubmit = (event) => {
     event.preventDefault();
     // pass the state variable values to a new movie object
+    const newMovie = { title, director, IMDBRating, hasOscars };
+    console.log("newMovie", newMovie);
+    addNewMovie(newMovie);
     // add the new movie to the state variable "movies"
+    // Reset the state
+
+    setTitle("");
+    setDirector("");
+    setIMDBRating(5);
+    setHasOscars(true);
   };
   return (
     <div className="AddMovie">
@@ -17,16 +27,36 @@ function AddMovie(props) {
 
       <form onSubmit={handleSubmit}>
         <label>Title: </label>
-        <input type="text" name="title" value={title} />
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
 
         <label>Director: </label>
-        <input type="text" name="director" value={director} />
+        <input
+          type="text"
+          name="director"
+          value={director}
+          onChange={(event) => setDirector(event.target.value)}
+        />
 
         <label>IMDB Rating: </label>
-        <input type="number" name="IMDBRating" value={IMDBRating} />
+        <input
+          type="number"
+          name="IMDBRating"
+          value={IMDBRating}
+          onChange={(event) => setIMDBRating(event.target.value)}
+        />
 
         <label>Won Oscars: </label>
-        <input type="checkbox" name="hasOscars" checked={hasOscars} />
+        <input
+          type="checkbox"
+          name="hasOscars"
+          checked={hasOscars}
+          onChange={(event) => setHasOscars(!hasOscars)}
+        />
 
         <button type="submit">Add a Movie</button>
       </form>
--- m3/w2/d2/react-forms-recordings/src/components/MovieList.jsx
+++ m3/w2/d2/react-forms-recordings/src/components/MovieList.jsx
@@ -2,13 +2,38 @@ import { useState } from "react";
 import moviesDataJSON from "../movies-data.json";
 import MovieCard from "./MovieCard";
 import AddMovie from "./AddMovie";
+import FilterMovies from "./FilterMovies";
 
 function MovieList() {
   const [movies, setMovies] = useState(moviesDataJSON);
   const [showForm, setShowForm] = useState(false);
+  const [moviesData, setMoviesData] = useState(moviesDataJSON); //this state variable will only be updated to add movies
 
+  const addNewMovie = (newMovie) => {
+    //create a new array
+    const updatedMovies = [...movies, newMovie];
+    const updatedMoviesData = [...moviesData, newMovie];
+
+    setMovies(updatedMovies);
+    setMoviesData(updatedMoviesData);
+  };
+
+  const filterMovieList = (str) => {
+    let filteredMovies;
+
+    if (str === "All") {
+      filteredMovies = moviesData;
+    } else {
+      filteredMovies = moviesData.filter((movie) => {
+        return movie.title[0].toLowerCase() === str.toLowerCase();
+      });
+    }
+
+    setMovies(filteredMovies);
+  };
   return (
     <div>
+      <FilterMovies filterMovieList={filterMovieList} />
       <h2>Movie List</h2>
 
       {movies.map((movie) => {
@@ -18,7 +43,7 @@ function MovieList() {
       <button onClick={() => setShowForm(!showForm)}>
         {showForm ? "Hide Add Movie Form" : "Display Add Movie form"}
       </button>
-      {showForm && <AddMovie />}
+      {showForm && <AddMovie addNewMovie={addNewMovie} />}
     </div>
   );
 }
